7be9de21dbf041414ad1f710b6e7b85cde44fc6b
MartaAnetaSzymanska <szymanska.ma@gmail.com>
2024-07-19 16:38:29 +0200

    remove_old_functions

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,19 +5,6 @@ import { ContactList } from "./ContactList/ContactList";
 import { nanoid } from "nanoid";
 
 export const App = () => {
-  const filterContact = () => {
-    const filterLowerCase = filter.toLowerCase();
-    return contacts.filter((contact) =>
-      contact.name.toLowerCase().includes(filterLowerCase),
-    );
-  };
-
-  const deleteContact = (id) => {
-    setContacts((prevState) =>
-      prevState.filter((contact) => contact.id !== id),
-    );
-  };
-
   return (
     <>
       <h1>Phonebook</h1>
